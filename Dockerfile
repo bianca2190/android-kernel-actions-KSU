f74aeabde8ca5eeaf32ea418a3e4d256c60d6bc4
sen-tenz <129577497+sen-tenz@users.noreply.github.com>
2023-04-03 12:37:09 +0000

    trying to add neutron clang support ;(

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/library/ubuntu:jammy
+FROM ghcr.io/neutron-projects/docker-image:arch-neutron
 ENV DEBIAN_FRONTEND noninteractive
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -35,12 +35,11 @@ name="${NAME:-$repo_name}"
 python_version="${PYTHON_VERSION:-3}"
 
 msg "Updating container..."
-apt update && apt upgrade -y
+pacman -Syu --noconfirm
 msg "Installing essential packages..."
-apt install -y --no-install-recommends git make bc bison openssl \
-    curl zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget \
-    device-tree-compiler ca-certificates python3 python2 xz-utils
-ln -sf "/usr/bin/python${python_version}" /usr/bin/python
+pacman -Sy --noconfirm kmod libelf-dev libssl-dev libtfm-dev \
+    device-tree-compiler ca-certificates xz-utils
+
 set_output hash "$(cd "$kernel_path" && git rev-parse HEAD || exit 127)"
 msg "Installing toolchain..."
 if [[ $arch = "arm64" ]]; then
@@ -53,7 +52,7 @@ if [[ $arch = "arm64" ]]; then
         make_opts=""
         host_make_opts=""
 
-        if ! apt install -y --no-install-recommends gcc-"$ver_number" g++-"$ver_number" \
+        if ! pacman -Sy --noconfirm gcc-"$ver_number" g++-"$ver_number" \
             gcc-"$ver_number"-aarch64-linux-gnu gcc-"$ver_number"-arm-linux-gnueabi; then
             err "Compiler package not found, refer to the README for details"
             exit 1
@@ -84,7 +83,7 @@ if [[ $arch = "arm64" ]]; then
             host_make_opts="HOSTCC=clang HOSTCXX=clang++ HOSTLD=ld.lld HOSTAR=llvm-ar"
         fi
 
-        if ! apt install -y --no-install-recommends clang-"$ver_number" \
+        if ! pacman -Sy --noconfirm clang-"$ver_number" \
             lld-"$ver_number" llvm-"$ver_number" $additional_packages; then
             err "Compiler package not found, refer to the README for details"
             exit 1
@@ -99,6 +98,38 @@ if [[ $arch = "arm64" ]]; then
         done
 
         export CLANG_TRIPLE="aarch64-linux-gnu-"
+        export CROSS_COMPILE="aarch64-linux-gnu-"
+        export CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
+    elif [[ $compiler = neutron-clang/* ]]; then
+        ver="${compiler/neutron-clang\/}"
+        ver_number="${ver/\/binutils}"
+
+        binutils="$([[ $ver = */binutils ]] && echo true || echo false)"
+
+        mkdir -p "$workdir"/"neutron-clang"-"${ver_number}"
+        cd "$workdir"/"neutron-clang"-"${ver_number}"
+
+        echo "Downloading neutron-clang version - $ver_number"
+        
+        if ! bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S="$ver_number"; then
+            err "Failed downloading toolchain, refer to the README for details"
+            exit 1
+        fi
+
+        if $binutils; then
+            make_opts="CC=clang"
+            host_make_opts="HOSTCC=clang HOSTCXX=clang++"
+        else
+            make_opts="CC=clang LD=ld.lld NM=llvm-nm AR=llvm-ar STRIP=llvm-strip OBJCOPY=llvm-objcopy"
+            make_opts+=" OBJDUMP=llvm-objdump READELF=llvm-readelf LLVM=1 LLVM_IAS=1"
+            host_make_opts="HOSTCC=clang HOSTCXX=clang++ HOSTLD=ld.lld HOSTAR=llvm-ar"
+        fi
+
+        cd "$workdir"/"neutron-clang"-"${ver_number}" || exit 127
+        neutron_path="$(pwd)"
+        cd "$workdir"/"$kernel_path" || exit 127
+
+        export PATH="$neutron_path/bin:${PATH}"
         export CROSS_COMPILE="aarch64-linux-gnu-"
         export CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
     elif [[ $compiler = proton-clang/* ]]; then
@@ -124,7 +155,7 @@ if [[ $arch = "arm64" ]]; then
             host_make_opts="HOSTCC=clang HOSTCXX=clang++ HOSTLD=ld.lld HOSTAR=llvm-ar"
         fi
 
-        apt install -y --no-install-recommends libgcc-10-dev || exit 127
+        pacman -Sy --noconfirm libgcc-10-dev || exit 127
         extract_tarball /tmp/proton-clang-"${ver_number}".tar.gz /
         cd /proton-clang-"${ver_number}"* || exit 127
         proton_path="$(pwd)"
@@ -183,7 +214,7 @@ if [[ $arch = "arm64" ]]; then
             host_make_opts="HOSTCC=clang HOSTCXX=clang++ HOSTLD=ld.lld HOSTAR=llvm-ar"
         fi
 
-        apt install -y --no-install-recommends libgcc-10-dev || exit 127
+        pacman -Sy --noconfirm libgcc-10-dev || exit 127
 
         export PATH="/aosp-clang/bin:/aosp-gcc-arm64/bin:/aosp-gcc-arm/bin:/aosp-gcc-host/bin:$PATH"
         export CLANG_TRIPLE="aarch64-linux-gnu-"
