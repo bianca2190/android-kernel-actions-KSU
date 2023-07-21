628cdc17aee63836e47037c306daea0d29e77f77
bianca2190 <109406227+bianca2190@users.noreply.github.com>
2023-07-21 13:38:45 +0700

    Update prompt-ksu.sh

--- prompt-ksu.sh
+++ prompt-ksu.sh
@@ -1,11 +1,18 @@
-#!/usr/bin/expect
+#!/bin/bash
 
-# Membuat spawn untuk menjalankan perintah make
-spawn make O=out $arch_opts $make_opts $host_make_opts -j"$(nproc --all)" prepare
+# Fungsi untuk mengecek apakah prompt muncul
+check_prompt() {
+  tail -n 0 -F log.txt | grep -q "KernelSU function support (KSU) \[Y/n/?\]"
+}
 
-# Menggunakan expect untuk menangani prompt dan memberikan input "y"
-expect "KernelSU function support (KSU) \[Y/n/?\]"
-send "y\r"
+# Memulai tail pada file log untuk menunggu munculnya prompt
+check_prompt &
 
-# Tunggu hingga proses selesai
-expect eof
+# Menunggu munculnya prompt
+wait $!
+
+# Memberikan input "y" dan menambahkan karakter Enter
+echo "y" > log.txt
+
+# Menjalankan perintah make dengan log output
+make O=out $arch_opts $make_opts $host_make_opts -j"$(nproc --all)" prepare
