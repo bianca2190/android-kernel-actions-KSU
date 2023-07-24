6aac611a821b7ad630a1183e7d46b39aead87031
bianca2190 <109406227+bianca2190@users.noreply.github.com>
2023-07-24 13:54:59 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,7 @@ apt update && apt upgrade -y
 msg "Installing essential packages..."
 apt install -y --no-install-recommends git make bc bison openssl \
     curl zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget \
-    device-tree-compiler ca-certificates python3 python2 xz-utils
+    device-tree-compiler ca-certificates python3 python2 xz-utils libxml2
 msg "Check space setelah..."
 df -h /
 ln -sf "/usr/bin/python${python_version}" /usr/bin/python
